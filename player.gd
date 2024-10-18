5b3ee1ebc2196972be57057ca108e3a4fcdd1cc3
NazaGonella <80429205+NazaGonella@users.noreply.github.com>
2024-10-18 18:53:18 -0300

    final movement

--- player.gd
+++ player.gd
@@ -4,37 +4,31 @@ extends CharacterBody2D
 @export var punch_area : Area2D
 @export var punch_cooldown : Timer
 
-const SPEED : float = 150.0
+const SPEED : float = 80.0
 const ROTATION_SPEED : float = 3.0
 
 func _physics_process(delta):
-	
 	var velocity_length = handle_movement()
-	if velocity_length == 0:
-		handle_rotation(delta, ROTATION_SPEED)
-	else:
-		handle_rotation(delta, ROTATION_SPEED / 2)
+	if velocity_length != 0:
+		handle_rotation()
 	handle_actions()
 	
 	move_and_slide()
 
 func handle_movement():
-	var orientation = Vector2(cos(rotation - PI/2), sin(rotation - PI/2))
-	#var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
-	var direction = Input.get_axis("move_up", "move_down")
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+	#var direction = Input.get_axis("move_up", "move_down")
 	if direction:
-		#velocity = direction * speed
-		velocity = direction * orientation * SPEED
+		velocity = direction * SPEED
+		#velocity = direction * orientation * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	return velocity.length()
 
-func handle_rotation(delta, rotation_speed):
-	var mouse_position = get_global_mouse_position()
-	var direction = Input.get_axis("rotate_left", "rotate_right")
-	#rotation = atan2((global_position.x - mouse_position.x), -(global_position.y - mouse_position.y))
-	rotation += rotation_speed * delta * direction
+func handle_rotation():
+	#var direction = Input.get_axis("rotate_left", "rotate_right")
+	rotation = atan2((velocity.x), -(velocity.y))
 
 func handle_actions():
 	if Input.is_action_just_pressed("punch"):
