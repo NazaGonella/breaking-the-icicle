828e8b1f90bb0d32fe6213efb421fe847f58a8e9
NazaGonella <80429205+NazaGonella@users.noreply.github.com>
2024-10-18 20:52:40 -0300

    fog y tilemap

--- player.gd
+++ player.gd
@@ -1,42 +1,34 @@
 extends CharacterBody2D
 
 @export var animated_sprite : AnimatedSprite2D
-@export var punch_area : Area2D
-@export var punch_cooldown : Timer
+@export var campo_de_vision : Sprite2D
 
-const SPEED : float = 80.0
-const ROTATION_SPEED : float = 3.0
+const SPEED : float = 40.0
+
+func _ready():
+	campo_de_vision.visible = true
 
 func _physics_process(delta):
 	var velocity_length = handle_movement()
 	if velocity_length != 0:
 		handle_rotation()
 	handle_actions()
-	
 	move_and_slide()
 
 func handle_movement():
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
-	#var direction = Input.get_axis("move_up", "move_down")
 	if direction:
 		velocity = direction * SPEED
-		#velocity = direction * orientation * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	return velocity.length()
 
 func handle_rotation():
-	#var direction = Input.get_axis("rotate_left", "rotate_right")
 	rotation = atan2((velocity.x), -(velocity.y))
 
 func handle_actions():
 	if Input.is_action_just_pressed("punch"):
 		animated_sprite.frame = 0
 		animated_sprite.play()
-		punch_action()
-
-func punch_action():
-	for body in punch_area.get_overlapping_bodies():
-		print("Punched ", body)
 
