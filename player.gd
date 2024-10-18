c7762be5f5704e51291d306f3aafb68fbe04c171
NazaGonella <80429205+NazaGonella@users.noreply.github.com>
2024-10-18 18:44:45 -0300

    movimiento con rotación: a cambiar

--- player.gd
+++ player.gd
@@ -4,13 +4,16 @@ extends CharacterBody2D
 @export var punch_area : Area2D
 @export var punch_cooldown : Timer
 
-@export var speed : float = 300.0
-@export var rotation_speed : float = 4.0
+const SPEED : float = 150.0
+const ROTATION_SPEED : float = 3.0
 
 func _physics_process(delta):
 	
-	handle_movement()
-	handle_rotation(delta)
+	var velocity_length = handle_movement()
+	if velocity_length == 0:
+		handle_rotation(delta, ROTATION_SPEED)
+	else:
+		handle_rotation(delta, ROTATION_SPEED / 2)
 	handle_actions()
 	
 	move_and_slide()
@@ -21,12 +24,13 @@ func handle_movement():
 	var direction = Input.get_axis("move_up", "move_down")
 	if direction:
 		#velocity = direction * speed
-		velocity = direction * orientation * speed
+		velocity = direction * orientation * SPEED
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.y = move_toward(velocity.y, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	return velocity.length()
 
-func handle_rotation(delta):
+func handle_rotation(delta, rotation_speed):
 	var mouse_position = get_global_mouse_position()
 	var direction = Input.get_axis("rotate_left", "rotate_right")
 	#rotation = atan2((global_position.x - mouse_position.x), -(global_position.y - mouse_position.y))
