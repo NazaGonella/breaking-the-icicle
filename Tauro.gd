a8c4a902f977be47a657ed115fd2a5714a2c7e73
myopian1 <leporacelara@gmail.com>
2024-10-19 10:22:07 -0300

    hilo_fix

--- Tauro.gd
+++ Tauro.gd
@@ -1,13 +1,11 @@
 extends CharacterBody2D
 
-var griddedhistory : Array = [Vector2i(0,0)]
 var griddedpos: Vector2i = Vector2i(0,0)
 
 func _physics_process(delta):
 	global_position=get_global_mouse_position()
-	griddedpos= Vector2i(global_position.x/32,global_position.y/32)
-	if griddedpos!=griddedhistory[-1]:
-		griddedhistory.append(griddedpos)
+	griddedpos= Vector2i(global_position.x/64,global_position.y/64)
+	#print(griddedpos)
 	velocity.y = 100
 	velocity.x = 100
 	
--- soga.gd
+++ soga.gd
@@ -1,28 +1,25 @@
 extends Node2D
-
 @export var player:CharacterBody2D
 @export var tauro:CharacterBody2D
-
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#print(tauro.griddedpos)
 	for i in range(len(player.griddedhistory)):
 		if tauro.griddedpos==player.griddedhistory[i]:
 			#print("el tauro se cruza con el hilo en",tauro.griddedpos, "globalhistory:",player.globalhistory)
 			for j in range(len(player.globalhistory)-1,0,-1):
+				#print(len(player.globalhistory))
 				#print(j)
-				var dist : Vector2 = Vector2(player.globalhistory[j]/32)-Vector2(tauro.griddedpos)
-				print(dist)
+				#print(dist)
 				#print(Vector2(player.globalhistory[j]))
 				#print(dist==player.globalhistory[j])
-				if dist.x<32 and dist.x>=0 and dist.y<32 and dist.y>=0:
-					player.globalhistory=player.globalhistory.slice(j,len(player.globalhistory)-1)
+				if share_tile(tauro.griddedpos,player.globalhistory[j]/64):
+					print("tauro:",tauro.griddedpos,"player:",player.globalhistory[j]/64)
+					player.globalhistory=player.globalhistory.slice(j+1,len(player.globalhistory)-1)
+					player.griddedhistory=player.griddedhistory.slice(i+1,len(player.griddedhistory)-1)
 					#print(player.globalhistory)
 					break
 			break
@@ -37,3 +34,8 @@ func _draw():
 	for coord in player.globalhistory:
 		string.append(Vector2(coord[0], coord[1]))
 	draw_polyline(string, Color.DARK_RED, 3)
+	
+func share_tile(vector1 : Vector2, vector2 : Vector2):
+	var dist : Vector2 = (vector2)-(vector1)
+	if dist.x<1 and dist.x>=0 and dist.y<1 and dist.y>=0:
+		return true
