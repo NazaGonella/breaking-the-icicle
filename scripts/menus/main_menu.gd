1b6f77f4b8785af79f39785d300c0f87aab532c4
lucas <lromanoabdo@udesa.edu.ar>
2024-10-19 02:21:25 -0300

    añadiendo los sonidos al menu principal

--- scripts/menus/main_menu.gd
+++ scripts/menus/main_menu.gd
@@ -1,6 +1,8 @@
 extends Control
 
 @onready var start_game_button =$MarginContainer/VBoxContainer/ButtonsVBox/StartGame 
+@onready var click = $button_pressing
+@onready var focus = $button_switching
 
 signal start_game()
 
@@ -10,12 +12,34 @@ func _ready():
 	start_game_button.grab_focus()
 	
 func _on_start_game_pressed():
+	click.play()
 	start_game.emit()
 	hide()
 
 func _on_quit_game_button_pressed():
+	click.play()
 	get_tree().quit()
 
 func _on_options_button_pressed():
+	click.play()
 	options_menu.emit()
 	hide()
+
+func _on_start_game_focus_entered():
+	focus.play()
+
+func _on_options_button_focus_entered():
+	focus.play()
+
+func _on_quit_game_button_focus_entered():
+	focus.play()
+
+
+func _on_start_game_mouse_entered():
+	focus.play()
+
+func _on_options_button_mouse_entered():
+	focus.play()
+
+func _on_quit_game_button_mouse_entered():
+	focus.play()
