028f61eeb762fd7c7fbda96d2d4255ec5c5863f2
myopian1 <leporacelara@gmail.com>
2024-10-19 22:12:16 -0300

    soga_rng

--- scripts/soga.gd
+++ scripts/soga.gd
@@ -2,8 +2,10 @@ extends Node2D
 @export var player:CharacterBody2D
 @export var tauro:CharacterBody2D
 
+var rng = RandomNumberGenerator.new()
 var restos: Array = []
 var espejos: Array = [Vector2(0,0),Vector2(-480,224),Vector2(0,480),Vector2(480,256),Vector2(480,-224),Vector2(-480,-256),Vector2(0,-480)]
+
 
 var ANCHO_SOGA : float = 1
 
@@ -22,11 +24,11 @@ func _process(delta):
 				#print(Vector2(player.globalhistory[j]))
 				#print(dist==player.globalhistory[j])
 				if share_tile(tauro.griddedpos,player.globalhistory[j]/32):
-					#print("tauro:",tauro.griddedpos,"player:",player.globalhistory[j]/32)
-					restos.append(player.globalhistory[j])
+					if rng.randi_range(0, 1):
+						restos.append(player.globalhistory[j])
 					player.tphistory.append(player.globalhistory[j+1])
 					player.globalhistory=player.globalhistory.slice(0,j-5)+player.globalhistory.slice(j+1,len(player.globalhistory)-1)
-					player.griddedhistory=player.griddedhistory.slice(i+1,len(player.griddedhistory)-1)
+					player.griddedhistory=player.griddedhistory.slice(0,i)+player.griddedhistory.slice(i+1,len(player.griddedhistory)-1)
 
 					#print(player.globalhistory)
 					break
